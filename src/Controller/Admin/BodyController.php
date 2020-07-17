51f791303a357756e60f368db6352a1f7e105e75
hubert <hubert@hubert-ubuntu>
2020-07-17 14:24:27 +0200

    update project repository for codingame

--- src/Controller/Admin/BodyController.php
+++ src/Controller/Admin/BodyController.php
@@ -1,53 +1,112 @@
 <?php
+/* src/Controller/Admin/BodyController.php */
 
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\Home\BodyRepository as BodyRep;
+use App\Entity\Body;
+use App\Form\BodyFormType;
 
 class BodyController extends AbstractController
 {
     
+    private $body_rep;
+    private $body_edit;
+ 
+    public function __construct(BodyRep $body_rep, BodyFormType $body_form)
+    {
+        $this->body_rep = $body_rep;
+        $this->body_edit = $body_form;
+    }
+    
+    /* ********************************************************************** */
+    
     /**
+     * LISTING
      * @Method({"GET"})
      * @Route("/admin/body/listing", name="admin_body_listing")
      */
-    public function listing(BodyRep $body_rep)
+    public function listing()
     {
-        $rows = $body_rep->findAll();
+        $body = $this->body_rep->findAll();
         return $this->render('admin/listing/body.html.twig', [
             'url' => 'admin - listing',
-            'rows' => $rows,
+            'rows' => $body,
         ]);
     }
     
     /**
+     * EDIT
      * @Method({"GET"})
      * @Route("/admin/body/edit/{id}", name="admin_body_edit")
      */
-    public function edit(BodyRep $body_rep, $id)
+    public function edit(Request $request, $id)
     {
-        $body = $body_rep->find($id);
-        $form = $this->createForm(\App\Form\BodyFormType::class, $body);
-        return $this->render('admin/form/body.html.twig', [
-            'url' => 'admin - edit',
-            'form_edit' => $form->createView(),
-        ]);
+        $body = $this->body_rep->find($id);
+        $form = $this->createForm(BodyFormType::class, $body);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()):
+            $rows = $this->body_rep->findAll();
+            $datas = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($datas);
+            $em->flush();
+            return $this->render('admin/listing/body.html.twig', [
+                'url' => 'admin - listing',
+                'rows' => $rows,
+            ]);
+        else:
+            return $this->render('admin/form/body.html.twig', [
+                'url' => 'admin - edit',
+                'form_edit' => $form->createView(),
+            ]);
+        endif;
     }
     
     /**
+     * NEW
+     * @Method({"GET"})
+     * @Route("/admin/body/new", name="admin_body_new")
+     */
+    public function new(Request $request)
+    {
+        $body = new Body();
+        $form = $this->createForm(BodyFormType::class, $body);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()):
+            $rows = $this->body_rep->findAll();
+            $datas = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($datas);
+            $em->flush();
+            return $this->redirectToRoute('admin_body_listing',[
+                'url' => 'admin - listing',
+                'rows' => $rows,
+            ]);
+        else:
+            return $this->render('admin/form/body.html.twig', [
+                'url' => 'admin - new',
+                'form_edit' => $form->createView(),
+            ]);
+        endif;
+    }
+    
+    /**
+     * DELETE
      * @Method({"GET"})
      * @Route("/admin/body/delete/{id}", name="admin_body_delete")
      */
-    public function delete(BodyRep $body_rep, $id)
+    public function delete($id)
     {
-        $rows = $body_rep->find($id);
-        return $this->render('admin/listing/body.html.twig', [
-            'url' => 'admin - delete',
-            'rows' => $rows,
-        ]);
+        $body = $this->body_rep->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($body);
+        $em->flush();
+        return $this->redirectToRoute('admin_body_listing');
     }
     
 }
--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -5,18 +5,33 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\Practice\CategoryRepository as CategoryRep;
+use App\Form\CategoryFormType;
+use App\Entity\Category;
 
 class CategoryController extends AbstractController
 {
     
+    private $category_rep;
+    private $category_edit;
+ 
+    public function __construct(CategoryRep $category_rep, CategoryFormType $category_form)
+    {
+        $this->category_rep = $category_rep;
+        $this->category_edit = $category_form;
+    }
+    
+    /* ********************************************************************** */
+    
     /**
+     * LISTING
      * @Method({"GET"})
      * @Route("/admin/category/listing", name="admin_category_listing")
      */
-    public function listing(CategoryRep $category_rep)
+    public function listing()
     {
-        $rows = $category_rep->findAll();
+        $rows = $this->category_rep->findAll();
         return $this->render('admin/listing/category.html.twig', [
             'url' => 'admin - listing',
             'rows' => $rows,
@@ -24,30 +39,73 @@ class CategoryController extends AbstractController
     }
     
     /**
+     * EDIT
      * @Method({"GET"})
      * @Route("/admin/category/edit/{id}", name="admin_category_edit")
      */
-    public function edit(CategoryRep $category_rep, $id)
+    public function edit(Request $request, $id)
     {
-        $category = $category_rep->find($id);
-        $form = $this->createForm(\App\Form\CategoryFormType::class, $category);
-        return $this->render('admin/form/category.html.twig', [
-            'url' => 'admin - edit',
-            'form_edit' => $form->createView(),
-        ]);
+        $category = $this->category_rep->find($id);
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()):
+            $rows = $this->category_rep->findAll();
+            $datas = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($datas);
+            $em->flush();
+            return $this->render('admin/listing/category.html.twig', [
+                'url' => 'admin - listing',
+                'rows' => $rows,
+            ]);
+        else:
+            return $this->render('admin/form/category.html.twig', [
+                'url' => 'admin - edit',
+                'form_edit' => $form->createView(),
+            ]);
+        endif;
     }
     
     /**
+     * NEW
+     * @Method({"GET"})
+     * @Route("/admin/category/new", name="admin_category_new")
+     */
+    public function new(Request $request)
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()):
+            $rows = $this->category_rep->findAll();
+            $datas = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($datas);
+            $em->flush();
+            return $this->redirectToRoute('admin_category_listing',[
+                'url' => 'admin - listing',
+                'rows' => $rows,
+            ]);
+        else:
+            return $this->render('admin/form/category.html.twig', [
+                'url' => 'admin - new',
+                'form_edit' => $form->createView(),
+            ]);
+        endif;
+    }
+    
+    /**
+     * DELETE
      * @Method({"GET"})
      * @Route("/admin/category/delete/{id}", name="admin_category_delete")
      */
-    public function delete(CategoryRep $category_rep, $id)
+    public function delete($id)
     {
-        $rows = $category_rep->find($id);
-        return $this->render('admin/listing/category.html.twig', [
-            'url' => 'admin - delete',
-            'rows' => $rows,
-        ]);
+        $category = $this->category_rep->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($category);
+        $em->flush();
+        return $this->redirectToRoute('admin_category_listing');
     }
     
 }
