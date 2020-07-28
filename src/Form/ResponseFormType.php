2a16d9db9992bdbe0ff8bc605bb70cb5cf0d0f6f
hubert <hubert@hubert-ubuntu>
2020-07-28 14:24:57 +0200

    update project repository for codingame

--- src/Form/ResponseFormType.php
+++ src/Form/ResponseFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Question;
 use App\Entity\Response;
 
 class ResponseFormType extends AbstractType
@@ -17,7 +18,7 @@ class ResponseFormType extends AbstractType
     {
         $builder
             ->add('question',EntityType::class, [
-                'class'=>'App:Question',
+                'class' => Question::class,
                 'query_builder'=>function(\Doctrine\ORM\EntityRepository $er){
                      return $er->createQueryBuilder('q')->orderBy('q.title','ASC');
                 },
@@ -25,7 +26,6 @@ class ResponseFormType extends AbstractType
                 'attr' => ['class' => 'form-row'],
             ])
             ->add('answer', TextareaType::class, [
-                'required' => true,
                 'attr' => ['class' => 'form-row'],
             ])
             ->add('submit', SubmitType::class, [
